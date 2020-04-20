5298018152e33676e849f826ddbdd54d1e1c5912
roguejuan44 <elitethan@gmail.com>
2020-04-20 13:58:57 -0600

    Got the JS to finally display the date

--- indexjs.js
+++ indexjs.js
@@ -1,6 +1,7 @@
-var year = new Date();
-document.getElementById("currentyear").innerHTML = year.getFullYear();
+    var year = new Date();
+    document.getElementById("currentyear").textContent = year.getFullYear();
 
-var lastmod = document.lastModified;
-const date = new Date(document.lastModified);
-document.getElementById("timedisp").textContent = (date.getMonth() + 1) + "/" + (date.getDay() + 1) + "/" + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    var lastmod = document.lastModified;
+    const date = new Date(document.lastModified);
+    document.getElementById("timedisp").textContent = (date.getMonth() + 1) + "/" + (date.getDay() + 1) 
+        + "/" + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
