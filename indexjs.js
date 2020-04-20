07eb38cdf8c9c6df3ce1c7c69e3106a894741b7e
roguejuan44 <elitethan@gmail.com>
2020-04-20 13:18:41 -0600

    js

--- indexjs.js
+++ indexjs.js
@@ -1,4 +1,6 @@
+var year = new Date();
+document.getElementById("currentyear").innerHTML = year.getFullYear();
+
 var lastmod = document.lastModified;
 const date = new Date(document.lastModified);
-const disp = (date.getMonth() + 1) + "/" + (date.getDay() + 1) + "/" + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-document.getElementById("time").textContent = disp;
+document.getElementById("timedisp").textContent = (date.getMonth() + 1) + "/" + (date.getDay() + 1) + "/" + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
