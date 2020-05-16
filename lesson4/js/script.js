4c21e61404a599fdf3eccf4c9f3c815d677384f5
roguejuan44 <elitethan@gmail.com>
2020-05-16 09:29:35 -0600

    time

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
     document.getElementById("dropdown").classList.toggle("hide");
 }
+
+window.onload = function time() {
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wedneday", "Thursday", "Friday", "Saturday"]
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var date = new Date();
+    var dayofweek = days[date.getDay()];
+    var monthofyear = months[date.getMonth()];    
+    var fulldate = dayofweek + ", " + date.getDate() + " " + monthofyear + " " + date.getFullYear();
+    document.getElementById("datetime").innerHTML = fulldate;
+    
+    }
