99bed6bad709ba3c741e49592186153a74fafd39
roguejuan44 <elitethan@gmail.com>
2020-05-25 10:52:04 -0600

    wk6

---
+++ lesson6/js/script.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+    document.getElementById("dropdown").classList.toggle("hide");
+}
+
+WebFont.load({
+    google: {
+      families: [
+         'Fredoka One', 'Jost'
+      ]
+    }
+  });
+
+window.onload = function time() {
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wedneday", "Thursday", "Friday", "Saturday"]
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var date = new Date();
+    var dayofweek = days[date.getDay()];
+    var monthofyear = months[date.getMonth()];    
+    var fulldate = dayofweek + ", " + date.getDate() + " " + monthofyear + " " + date.getFullYear();
+    document.getElementById("datetime").innerHTML = fulldate;
+    
+    /*------------------------------*/
+    
+    const banner = document.getElementById('picnic');
+    if(date.getDay() == 5){
+        banner.style.display = 'block';
+    }
+    else{
+        banner.style.display = 'none';
+}
+}
